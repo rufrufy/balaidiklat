4a7c0c094d21c7da32b04bd56fd28a7750353f37
ruffy <0pokegoid@gmail.com>
2026-07-01 09:59:05 +0700

    test https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS untuk semua URL yang di-generate Laravel ketika
+        // request datang via HTTPS (di belakang reverse proxy nginx/docker).
+        if (request()?->isSecure() || request()?->header('X-Forwarded-Proto') === 'https') {
+            URL::forceScheme('https');
+        }
     }
 }
